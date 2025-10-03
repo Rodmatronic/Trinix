edd413e0138278c3688293c27d2ccd5504e17f21
roddo <roddo@gmail.com>
2025-10-03 19:12:14 -0400

    version bump 0.02-3 -> 0.02-4

--- include/version.h
+++ include/version.h
@@ -1,4 +1,4 @@
 #define sys_name "FreeNIX"
-#define sys_release "0.02-3-release"
+#define sys_release "0.02-4-release"
   // version-candidate
-#define sys_version "0.02-3"
+#define sys_version "0.02-4"
