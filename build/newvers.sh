32994153da7b4a7d31fef38c0c5f6b6914ffc833
SuperPou <superpou1filip@gmail.com>
2025-10-30 21:00:04 +0100

    0.02-4 -> 0.03-1

--- build/newvers.sh
+++ build/newvers.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # update version info inside kernel
 cp include/version_master.h include/version.h
-sed -i "s|<version>|0.02-4|g" include/version.h; sed -i "s|<placeholder>|$(date) $(whoami)@$(hostname):$(pwd)|g" include/version.h
+sed -i "s|<version>|0.03-1|g" include/version.h; sed -i "s|<placeholder>|$(date) $(whoami)@$(hostname):$(pwd)|g" include/version.h
