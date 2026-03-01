f94fb8474d0d4e787db2836df632e811e36dbdef
roddo <roddo@gmail.com>
2026-03-01 18:50:38 -0500

    0.03a -> 0.03b

--- include/version.h
+++ include/version.h
@@ -1,5 +1,5 @@
 #include <config.h>
 
 #define sys_name "Trinix"
-#define sys_release "0.03a"
-#define sys_version "Trinix version 0.03a"
+#define sys_release "0.03b"
+#define sys_version "Trinix version 0.03b"
